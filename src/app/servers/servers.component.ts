f70ededb9a3777e5b76776abbb7f7a39870211f2
Mouhamadou <mouhagaye@localhost.localdomain>
2019-09-17 11:07:53 +0000

    Two Way biniding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = "No server was created !";
+  serverName = 'TestServer';
   constructor() {
     setTimeout(()=>{
       this.allowNewServer = true;
@@ -15,6 +16,9 @@ export class ServersComponent implements OnInit {
   }
   onCreateServer(){
     this.serverCreationStatus = "Server was created successfully !";
+  }
+  onUpdateServerName(event:Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
   ngOnInit() {
