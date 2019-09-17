abdaaf05807c88b26ee2e8fc8a77708c2ce638df
Mouhamadou <mouhagaye@localhost.localdomain>
2019-09-17 11:47:27 +0000

    Ngif statement

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,13 +9,15 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created !';
   serverName = 'TestServer';
+  serverCreated = false;
   constructor() {
     setTimeout(()=>{
       this.allowNewServer = true;
     },2000)
   }
   onCreateServer(){
-    return this.serverCreationStatus = 'Server was created successfully And his Name is ' + this.serverName;
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Server was created successfully And his Name is ' + this.serverName;
   }
   onUpdateServerName(event:Event){
     this.serverName = (<HTMLInputElement>event.target).value;
